19cce6d2ed21d52b194d66bba76ca5c15101b172
Trot-dms <trot.dms@gmail.com>
2016-12-06 21:01:07 +0100

    Functional done

--- Intermediate/src/main/java/sda/code/intermediate/part1/exercises/Functional.java
+++ Intermediate/src/main/java/sda/code/intermediate/part1/exercises/Functional.java
@@ -12,36 +12,42 @@ import sda.code.intermediate.part1.exercises.data.Product;
 public class Functional {
 
 	public List<Integer> squares(List<Integer> list) {
-		return list.stream().map(x->x*x).collect(Collectors.toList());
-		//throw new UnsupportedOperationException("Not implemented yet");
+		return list.stream().map(x -> x * x).collect(Collectors.toList());
+		// throw new UnsupportedOperationException("Not implemented yet");
 	}
 
 	public List<Integer> even(List<Integer> list) {
-		return list.stream().filter(x->x % 2 ==0).collect(Collectors.toList());
-		//throw new UnsupportedOperationException("Not implemented yet");
+		return list.stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
+		// throw new UnsupportedOperationException("Not implemented yet");
 	}
 
 	public Long countOdd(List<Integer> list) {
 		return list.stream().filter(x -> Math.abs(x) % 2 == 1).collect(Collectors.counting());
-		//throw new UnsupportedOperationException("Not implemented yet");
+		// throw new UnsupportedOperationException("Not implemented yet");
 	}
 
 	public Integer smallest(List<Integer> list) {
 		return list.stream().reduce(Math::min).orElseThrow(IllegalArgumentException::new);
-		//throw new UnsupportedOperationException("Not implemented yet");
+		// throw new UnsupportedOperationException("Not implemented yet");
 	}
 
 	private BigDecimal bruttoSum(Predicate<Product> policy, Function<BigDecimal, BigDecimal> tax,
 			List<Product> products) {
-		throw new UnsupportedOperationException("Not implemented yet");
+		return products.stream().map(p->policy.test(p) ? tax.apply(p.getPrice()) : p.getPrice())
+								.reduce(BigDecimal.ZERO, (x, y) -> x.add(y));
+								//.reduce(BigDecimal.ZERO, BigDecimal::add);
+		//throw new UnsupportedOperationException("Not implemented yet");
 	}
 
 	private Predicate<Product> taxOnItems() {
-		throw new UnsupportedOperationException("Not implemented yet");
+		return p -> p instanceof Item;
+		// throw new UnsupportedOperationException("Not implemented yet");
 	}
 
 	private Function<BigDecimal, BigDecimal> itemTax(BigDecimal tax) {
-		throw new UnsupportedOperationException("Not implemented yet");
+		final BigDecimal fullTax = tax.add(BigDecimal.ONE);
+		return t -> t.multiply(fullTax);
+		// throw new UnsupportedOperationException("Not implemented yet");
 	}
 
 	public BigDecimal cartBruttoSum(List<Product> products) {
